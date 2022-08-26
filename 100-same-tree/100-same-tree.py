6cd38d0b2651ea22f53d32951fed3bc648dd3dee
Iteoluwakishi <74046912+itebababatunde@users.noreply.github.com>
2022-08-26 03:51:40 +0100

    Time: 31 ms (93.87%), Space: 14 MB (29.86%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -8,22 +8,19 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         arr1 = []
         arr2 = []
-        self.inOrderTraversal(p, arr1)
-        self.inOrderTraversal(q, arr2)
-        print(arr1,arr2)
+        self.preOrderTraversal(p, arr1)
+        self.preOrderTraversal(q, arr2)
         return arr1==arr2
 
-        
-        
-    def inOrderTraversal(self, node, tree):
+    def preOrderTraversal(self, node, tree):
         
         if node is not None:
             tree.append(node.val)
             if node.left:
-                self.inOrderTraversal(node.left, tree)   
+                self.preOrderTraversal(node.left, tree)   
             elif node.left is None and node.right is not None:
                 tree.append('null')
-            self.inOrderTraversal(node.right, tree)
+            self.preOrderTraversal(node.right, tree)
    
             
         
